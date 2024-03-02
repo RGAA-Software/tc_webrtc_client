3ba637a8b4d4d45e9131454f5cbe7e8db9052ed2
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2024-03-02 17:57:15 +0800

    init
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

---
+++ 6167/include/third_party/blink/renderer/modules/printing/web_print_job.h
@@ -0,0 +1,34 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_PRINTING_WEB_PRINT_JOB_H_
+#define THIRD_PARTY_BLINK_RENDERER_MODULES_PRINTING_WEB_PRINT_JOB_H_
+
+#include "third_party/blink/renderer/modules/modules_export.h"
+#include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
+#include "third_party/blink/renderer/platform/heap/member.h"
+
+namespace blink {
+
+class ExecutionContext;
+class WebPrintJobAttributes;
+
+class MODULES_EXPORT WebPrintJob : public ScriptWrappable {
+  DEFINE_WRAPPERTYPEINFO();
+
+ public:
+  explicit WebPrintJob(ExecutionContext* execution_context);
+  ~WebPrintJob() override;
+
+  WebPrintJobAttributes* attributes() const { return attributes_; }
+
+  void Trace(Visitor* visitor) const override;
+
+ private:
+  Member<WebPrintJobAttributes> attributes_;
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_MODULES_PRINTING_WEB_PRINT_JOB_H_
