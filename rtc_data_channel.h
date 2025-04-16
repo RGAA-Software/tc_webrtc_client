c307baf6b7d7fe4b1ad1cb8fce84e40571614e86
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2025-04-17 00:27:53 +0800

    parse tlv message
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

--- rtc_data_channel.h
+++ rtc_data_channel.h
@@ -5,6 +5,7 @@
 #ifndef TC_DATA_CHANNEL_OBSERVER_H
 #define TC_DATA_CHANNEL_OBSERVER_H
 #include <atomic>
+#include <vector>
 #include "tc_common_new/webrtc_helper.h"
 
 namespace tc
@@ -13,6 +14,12 @@ namespace tc
     using OnDataCallback = std::function<void(const std::string&)>;
 
     class RtcConnection;
+
+    class NetTlvMessage {
+    public:
+        uint32_t type_{0};
+        std::string buffer_;
+    };
 
     class RtcDataChannel :  public webrtc::DataChannelObserver {
     public:
@@ -38,6 +45,7 @@ namespace tc
         std::atomic<int> pending_data_count_ = 0;  //只是表达排队数,并没有关心data发送是否成功
         bool exit_via_reconnect_ = false;
         OnDataCallback data_cbk_;
+        std::vector<NetTlvMessage> cached_messages_;
     };
 
 } // namespace dl
