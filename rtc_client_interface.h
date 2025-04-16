1ae3b7fa581b50487ceefc5533dce3b6bebeb0aa
hy <hy@gmail.com>
2025-04-16 20:05:03 +0800

    add webrtc data channel
    Signed-off-by: hy <hy@gmail.com>

---
+++ rtc_client_interface.h
@@ -0,0 +1,64 @@
+//
+// Created by RGAA on 13/04/2025.
+//
+
+#ifndef GAMMARAY_RTC_CLIENT_INTERFACE_H
+#define GAMMARAY_RTC_CLIENT_INTERFACE_H
+
+#include <string>
+#include <functional>
+
+namespace tc
+{
+
+    using OnLocalSdpSetCallback = std::function<void(const std::string&)>;
+    using OnLocalIceCallback = std::function<void(const std::string& ice, const std::string& mid, int sdp_mline_index)>;
+
+    using OnMediaMessageCallback = std::function<void(const std::string& msg)>;
+    using OnFtMessageCallback = std::function<void(const std::string& msg)>;
+
+    class RtcClientInterface {
+    public:
+        virtual bool Init() {
+            return false;
+        }
+
+        virtual bool OnRemoteSdp(const std::string& sdp) {
+            return false;
+        }
+
+        virtual bool OnRemoteIce(const std::string& ice, const std::string& mid, int32_t sdp_mline_index) {
+            return false;
+        }
+
+        virtual void SetOnLocalSdpSetCallback(OnLocalSdpSetCallback&& cbk) {
+            local_sdp_set_cbk_ = cbk;
+        }
+
+        virtual void SetOnLocalIceCallback(OnLocalIceCallback&& cbk) {
+            local_ice_cbk_ = cbk;
+        }
+
+        virtual void PostMediaMessage(const std::string& msg) = 0;
+
+        virtual void PostFtMessage(const std::string& msg) = 0;
+
+        virtual void SetMediaMessageCallback(const OnMediaMessageCallback& cbk) {
+            media_msg_cbk_ = cbk;
+        }
+
+        virtual void SetFtMessageCallback(const OnFtMessageCallback& cbk) {
+            ft_msg_cbk_ = cbk;
+        }
+
+    protected:
+        OnLocalSdpSetCallback local_sdp_set_cbk_;
+        OnLocalIceCallback local_ice_cbk_;
+
+        OnMediaMessageCallback media_msg_cbk_;
+        OnFtMessageCallback ft_msg_cbk_;
+    };
+
+}
+
+#endif //GAMMARAY_RTC_CLIENT_INTERFACE_H
---
+++ rtc_data_channel.cpp
@@ -0,0 +1,85 @@
+//
+// Created by RGAA
+//
+
+#include "rtc_data_channel.h"
+#include "tc_common_new/log.h"
+
+namespace tc
+{
+
+    std::shared_ptr<RtcDataChannel> RtcDataChannel::Make(RtcConnection* client, rtc::scoped_refptr<webrtc::DataChannelInterface> ch) {
+        return std::make_shared<RtcDataChannel>(client, ch);
+    }
+
+    RtcDataChannel::RtcDataChannel(RtcConnection* client, rtc::scoped_refptr<webrtc::DataChannelInterface> ch) {
+        this->client_ = client;
+        this->data_channel_ = ch;
+        this->data_channel_->RegisterObserver(this);
+    }
+
+    RtcDataChannel::~RtcDataChannel() {
+        this->data_channel_->UnregisterObserver();
+    }
+
+    void RtcDataChannel::OnStateChange() {
+        if (exit_via_reconnect_) {
+            return;
+        }
+        if (data_channel_->state() == webrtc::DataChannelInterface::kOpen) {
+            connected_ = true;
+            //context_->SendAppMessage(ClientEvtDataChannelReady{});
+
+        } else if (data_channel_->state() == webrtc::DataChannelInterface::kClosed) {
+            connected_ = false;
+            //context_->SendAppMessage(ClientEvtDataChannelClosed{});
+
+        }
+        LOGI("DataChannel state changed: {}, connected: {}", (int)data_channel_->state(), connected_);
+    }
+
+    void RtcDataChannel::OnMessage(const webrtc::DataBuffer &buffer) {
+        //LOGI("DataChannel Message: {}", buffer.size());
+        std::string data((char*)buffer.data.data(), buffer.size());
+        if (data_cbk_) {
+            data_cbk_(data);
+        }
+    }
+
+    void RtcDataChannel::OnBufferedAmountChange(uint64_t sent_data_size) {
+        DataChannelObserver::OnBufferedAmountChange(sent_data_size);
+        //LOGI("OnBufferAmount changed: {}", sent_data_size);
+    }
+
+    bool RtcDataChannel::IsConnected() {
+        return connected_;
+    }
+
+    void RtcDataChannel::SendData(const std::string& msg) {
+        if (!connected_) {
+            LOGW("DataChannel is not connected now.");
+            return;
+        }
+        ++pending_data_count_;
+        this->data_channel_->Send(webrtc::DataBuffer(msg));
+        //RLogI("send data via data channel: {}", msg.size());
+        --pending_data_count_;
+    }
+
+    int RtcDataChannel::GetPendingDataCount() {
+        return pending_data_count_;
+    }
+
+    void RtcDataChannel::Close() {
+        LOGI("RtcDataChannel Close!");
+        if (data_channel_) {
+            data_channel_->Close();
+        }
+        connected_ = false;
+    }
+
+    void RtcDataChannel::SetOnDataCallback(OnDataCallback&& cbk) {
+        data_cbk_ = cbk;
+    }
+
+} // namespace dl
